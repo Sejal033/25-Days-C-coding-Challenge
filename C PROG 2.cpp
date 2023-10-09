64fa64c7947adb7dd690565dd3f0f583d60a0a00
Sejal033 <147237595+Sejal033@users.noreply.github.com>
2023-10-09 18:56:03 +0530

    Add files via upload

---
+++ C PROG 2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	
+	printf("ENTER AN INTEGERS: ");
+	
+	scanf("%d",&a);
+	
+	printf("YOU ENTERED:%d",a);
+	
+	return 0;
+}
---
+++ C PROG 3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,s;
+	
+	printf("ENTER TWO INTEGERS: ");
+	
+	scanf("%d %d",&a,&b);
+	
+	s=a+b;
+	
+	printf("THE SUM OF TWO INTEGERS IS:%d",s);
+	
+	return 0;
+}
